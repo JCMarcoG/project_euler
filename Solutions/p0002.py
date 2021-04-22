f1d43e58f909eeb79cebeb9d7af5d6d4be919f39
juanbinartis <juanbinartis@binartis.com>
2021-04-22 21:32:42 +0200

    problems 0001 and 0002

---
+++ Solutions/p0002.py
@@ -0,0 +1,12 @@
+# Solution to Problem 0001
+
+def solution():
+    fib_list = [1, 2]
+    while fib_list[-1] < 4000000:
+        fib_list.append(fib_list[-2] + fib_list[-1])
+    ans = sum(x for x in fib_list[:-1] if (x % 2 == 0))
+    return(ans)
+
+
+if __name__ == "__main__":
+	print(solution())
